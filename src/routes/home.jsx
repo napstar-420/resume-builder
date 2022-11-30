47b1d3b451c34bbf5a495702341832baa86638b1
Zohaib Khan <zohaibkhan1162002@gmail.com>
2022-11-30 16:25:19 -0500

    create hero section in home.jsx

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import heroImage from '../assets/hero_image.png';
 
 export default function Home() {
   const [showNav, setShowNav] = useState(false);
   return (
-    <main>
-      <header className='bg-mainBlue flex justify-between items-center px-2 py-2'>
-        <h2 className='font-roboto font-semibold text-2xl text-white'>
+    <section>
+      <header className='bg-mainBlue flex justify-between items-center px-2 md:px-4 py-2 h-14'>
+        <h2 className='font-semibold text-2xl text-white'>
           Resume Builder
         </h2>
         <button
@@ -35,7 +36,29 @@ export default function Home() {
           </div>
         </nav>
       </header>
-    </main>
+      <main style={{minHeight: 'calc(100vh - 3.5rem)', gridTemplateRows: '1fr auto auto auto 1fr'}} className='w-srceen px-2 md:px-4 py-1 flex flex-col justify-center items-center text-center md:text-left md:grid grid-cols-2 place-items-start'>
+        <span></span>
+        <h1 className='text-4xl sm:text-5xl lg:text-7xl font-semibold text-darkBlue md:self-end'>
+            Free Online<br />
+            Resume Builder
+        </h1>
+        <ul className='flex items-center text-mainYellow gap-1 my-1 md:mb-8'>
+            <li className='sm:text-xl md:font-medium'>Free</li>
+            <span className='sm:text-xl md:font-medium'>-</span>
+            <li className='sm:text-xl md:font-medium'>Fast</li>
+            <span className='sm:text-xl md:font-medium'>-</span>
+            <li className='sm:text-xl md:font-medium'>Premium</li>
+        </ul>
+        <div className="hero_image_wrapper w-[250px] sm:w-[300px] lg:w-[400px] my-4 col-start-2 col-end-3 row-start-1 row-end-6 justify-self-center">
+            <img src={heroImage} alt='resume vector art' className="w-full"/>
+        </div>
+        <div className="flex flex-wrap items-center justify-center md:self-start">
+            <Link to='templates' className="border-btn md:ml-0">Templates</Link>
+            <Link to='edit/personal-info' className="normal-btn md:mr-0">Get Started</Link>
+        </div>
+        <span></span>
+      </main>
+    </section>
   );
 }
 
@@ -52,7 +75,7 @@ function MobileNav({ showNav, setShowNav }) {
       } w-screen min-h-screen p-2 sm:hidden flex flex-col justify-between transition-all`}
     >
       <div className='flex justify-between'>
-        <h3 className='text-2xl font-roboto font-semibold text-darkBlue bg-white'>
+        <h3 className='text-2xl font-semibold text-darkBlue bg-white'>
           Resume builder
         </h3>
         <button
