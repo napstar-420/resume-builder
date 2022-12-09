6a81de94f666600d7185cde67961fdcf1743558c
Zohaib Khan <zohaibkhan1162002@gmail.com>
2022-12-09 15:01:37 +0500

    Create user document in firestore on signup

--- src/routes/signup.jsx
+++ src/routes/signup.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Link, Form, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
-import { createUser, signUpWithGoogle } from "../signup";
+import {AiOutlineLoading3Quarters} from 'react-icons/ai';
+import { createUser, signUpWithGoogle, createUserDoc } from "../signup";
 
 export default function Signup() {
   const navigate = useNavigate();
@@ -16,115 +17,207 @@ export default function Signup() {
   const [password, setPassword] = useState("");
   const [confPassword, setConfPassword] = useState("");
   const [passwordsMatch, setPasswordsMatch] = useState(true);
-  const [accountCreated, setAccountCreated] = useState({bool: null, status: ''});
+  const [accountCreated, setAccountCreated] = useState('');
   // Handle Submit
-  async function handleSubmit (e) {
+  async function handleSubmit(e) {
+    setAccountCreated("in-process");
     e.preventDefault();
     if (password !== confPassword) {
+      setAccountCreated('idle');
       return;
     } else {
       const result = await createUser(email, password, name);
-      if(result === 'account-created') {
-        setAccountCreated({bool: true, status: result});
-        console.log('working');
-        navigate('/edit');
-      } else if (result === 'email-already-in-use') {
-        setAccountCreated({bool: false, status: result});
+      if (result !== "email-already-in-use" && result !== "error") {
+        await createUserDoc(result, name, email, password);
+        setAccountCreated("account-created");
+        console.log("working");
+        navigate("/edit");
+      } else if (result === "email-already-in-use") {
+        setAccountCreated(result);
       } else {
-        setAccountCreated({bool: true, status: 'somethings-wrong-happened'});
+        setAccountCreated(result);
       }
-    }
-  };
-
-  const handleGoogleSignUp = async() => {
-    const result = await signUpWithGoogle();
-    console.log(result);
-    if(result === 'logged-in') {
-      navigate('/edit');
     }
   }
 
+  const handleGoogleSignUp = async () => {
+    const result = await signUpWithGoogle();
+    console.log(result);
+    if (result === "logged-in") {
+      navigate("/edit");
+    }
+  };
+
   return (
     <div
-      className='w-full flex-1 grid place-items-center'
+      className='flex-1 grid place-items-center'
       style={backgroundStyle}
     >
-      <div className='bg-white w-full h-full sm:w-max sm:h-max sm:my-4 px-4 py-2 sm:py-8 sm:px-20 flex flex-col justify-center items-center sm:rounded sm:shadow-lg sm:shadow-gray-600 relative'>
-        <h1 className='text-5xl font-semibold text-mainBlue'>Sign up</h1>
-        <h3 className='text-base font-medium text-gray-500'>
+      <div className='bg-white h-full sm:w-max sm:h-max sm:my-4 px-4 py-2 sm:px-20 sm:py-8 flex flex-col sm:rounded sm:shadow-lg sm:shadow-gray-600 relative'>
+        <h1 className='text-5xl font-semibold text-mainBlue text-center'>Sign up</h1>
+        <h3 className='text-base font-medium text-gray-500 text-center'>
           To Resume Builder
         </h3>
         <Form onSubmit={(e) => handleSubmit(e)} className='my-4'>
-          <label htmlFor='name' className="formLabel">Name</label>
-          <input
-            type='text'
-            id='name'
-            name='name'
-            className='form_control mb-2'
-            placeholder='John Doe'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <label htmlFor='email' className='formLabel'>
-            Email
-          </label>
-          <p className='text-xs text-red-500'>
-            {accountCreated.status === 'email-already-in-use' ? 'Email already in use' : ''}
-          </p>
-          <input
-            type='email'
-            id='email'
-            name='email'
-            className='form_control mb-2 invalid:outline-red-500 valid:outline-green-500'
-            placeholder="johndoe@example.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label htmlFor='password' className='formLabel'>
-            Password
-          </label>
-          <input
-            type='password'
-            id='password'
-            name='password'
-            className='form_control mb-2 invalid:outline-red-500 valid:outline-green-500'
-            minLength='8'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <label htmlFor='conf_password' className='formLabel'>
-            Confirm Password
-          </label>
-          <p className='text-xs text-red-500'>
-            {passwordsMatch ? "" : "Passwords do not match"}
-          </p>
-          <input
-            type='password'
-            id='conf_password'
-            name='conf_password'
-            className={`form_control mb-2 ${
-              passwordsMatch
-                ? "valid:outline-green-500"
-                : "invalid:outline-red-500 "
-            }`}
-            required
-            minLength='8'
-            value={confPassword}
-            onChange={(e) => {
-              setConfPassword(e.target.value);
-              if (password === e.target.value) {
-                setPasswordsMatch(true);
-              } else {
-                setPasswordsMatch(false);
-              }
-            }}
-          />
-          <button type='submit' className='submit-btn w-full'>
-            Create an Account
-          </button>
+          {accountCreated !== "in-process" ? (
+            <>
+              <label htmlFor='name' className='formLabel'>
+                Name
+              </label>
+              <input
+                type='text'
+                id='name'
+                name='name'
+                className='form_control mb-2'
+                placeholder='John Doe'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <label htmlFor='email' className='formLabel'>
+                Email
+              </label>
+              <p className='text-xs text-red-500'>
+                {accountCreated === "email-already-in-use"
+                  ? "Email already in use"
+                  : ""}
+              </p>
+              <input
+                type='email'
+                id='email'
+                name='email'
+                className='form_control mb-2 invalid:outline-red-500 valid:outline-green-500'
+                placeholder='johndoe@example.com'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <label htmlFor='password' className='formLabel'>
+                Password
+              </label>
+              <input
+                type='password'
+                id='password'
+                name='password'
+                className='form_control mb-2 invalid:outline-red-500 valid:outline-green-500'
+                minLength='8'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <label htmlFor='conf_password' className='formLabel'>
+                Confirm Password
+              </label>
+              <p className='text-xs text-red-500'>
+                {passwordsMatch ? "" : "Passwords do not match"}
+              </p>
+              <input
+                type='password'
+                id='conf_password'
+                name='conf_password'
+                className={`form_control mb-2 ${
+                  passwordsMatch
+                    ? "valid:outline-green-500"
+                    : "invalid:outline-red-500 "
+                }`}
+                required
+                minLength='8'
+                value={confPassword}
+                onChange={(e) => {
+                  setConfPassword(e.target.value);
+                  if (password === e.target.value) {
+                    setPasswordsMatch(true);
+                  } else {
+                    setPasswordsMatch(false);
+                  }
+                }}
+              />
+              <button type='submit' className='submit-btn w-full'>
+                Create account
+              </button>
+            </>
+          ) : (
+            <>
+              <label htmlFor='name' className='formLabel'>
+                Name
+              </label>
+              <input
+                type='text'
+                id='name'
+                name='name'
+                className='form_control mb-2'
+                placeholder='John Doe'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                disabled
+              />
+              <label htmlFor='email' className='formLabel'>
+                Email
+              </label>
+              <p className='text-xs text-red-500'>
+                {accountCreated === "email-already-in-use"
+                  ? "Email already in use"
+                  : ""}
+              </p>
+              <input
+                type='email'
+                id='email'
+                name='email'
+                className='form_control mb-2 invalid:outline-red-500 valid:outline-green-500'
+                placeholder='johndoe@example.com'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                disabled
+              />
+              <label htmlFor='password' className='formLabel'>
+                Password
+              </label>
+              <input
+                type='password'
+                id='password'
+                name='password'
+                className='form_control mb-2 invalid:outline-red-500 valid:outline-green-500'
+                minLength='8'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                disabled
+              />
+              <label htmlFor='conf_password' className='formLabel'>
+                Confirm Password
+              </label>
+              <p className='text-xs text-red-500'>
+                {passwordsMatch ? "" : "Passwords do not match"}
+              </p>
+              <input
+                type='password'
+                id='conf_password'
+                name='conf_password'
+                className={`form_control mb-2 ${
+                  passwordsMatch
+                    ? "valid:outline-green-500"
+                    : "invalid:outline-red-500 "
+                }`}
+                required
+                disabled
+                minLength='8'
+                value={confPassword}
+                onChange={(e) => {
+                  setConfPassword(e.target.value);
+                  if (password === e.target.value) {
+                    setPasswordsMatch(true);
+                  } else {
+                    setPasswordsMatch(false);
+                  }
+                }}
+              />
+              <button type='submit' className='submit-btn w-full flex items-center justify-center'>
+                Creating <AiOutlineLoading3Quarters className="ml-2 animate-spin"/>
+              </button>
+            </>
+          )}
         </Form>
         <Link
           to='/login'
@@ -137,7 +230,10 @@ export default function Signup() {
           <div className='mx-2'>or</div>
           <div className='flex-1 h-1 bg-gray-300 rounded'></div>
         </div>
-        <button onClick={handleGoogleSignUp} className='px-16 py-2 border-2 border-gray-600 rounded text-lg font-semibold text-gray-600 flex items-center justify-center mb-4'>
+        <button
+          onClick={handleGoogleSignUp}
+          className='px-16 py-2 border-2 border-gray-600 rounded text-lg font-semibold text-gray-600 flex items-center justify-center mb-4'
+        >
           Sign up with Google <FcGoogle className='ml-2 text-xl' />
         </button>
         <p className='text-xs text-gray-400 '>
--- src/signup.js
+++ src/signup.js
@@ -1,15 +1,16 @@
 import { getAuth, createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import {app} from './firebaseConfig';
+import { doc, setDoc } from "firebase/firestore";
+import {app, db} from './firebaseConfig';
 
 
 export async function createUser(email, password, name) {
     const auth = getAuth(app);
     return await createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      updateProfile(auth.currentUser, {
+    .then(async(userCredentials) => {
+      await updateProfile(auth.currentUser, {
         displayName: name
       })
-      return 'account-created';
+      return userCredentials.user.uid;
     })
     .catch((error) => {
       if(error.code === 'auth/email-already-in-use') {
@@ -31,4 +32,67 @@ export function signUpWithGoogle() {
     console.log(error);
   })
   return result;
+}
+
+export async function createUserDoc(uid, name, email, password) {
+  await setDoc(doc(db, `users/${name + uid}`), {
+    email,
+    password,
+    name
+  })
+
+  await setDoc(doc(db, 'users', name + uid, 'data', 'personal_info'), {
+    fname: '',
+    sname: '',
+    email: '',
+    mNumber: '',
+    sNumber: '',
+    address: ''
+  })
+
+  await setDoc(doc(db, 'users', name + uid, 'data', 'work_history'), {
+    workHistory: [
+      {
+        jobTitle: '',
+        employer: '',
+        city: '',
+        country: '',
+        startDate: '',
+        endDate: '',
+        workExp: ''
+      }
+    ]
+  })
+  await setDoc(doc(db, 'users', name + uid, 'data', 'education'), {
+    education: [
+      {
+        schoolName: '',
+        schoolLocation: '',
+        degree: '',
+        fod: '',
+        startDate: '',
+        endDate: '',
+        eduExp: ''
+      }
+    ]
+  })
+  await setDoc(doc(db, 'users', name + uid, 'data', 'skills'), {
+    skills: [
+      {
+        skillName: '',
+        skillLevel: ''
+      }
+    ]
+  })
+  await setDoc(doc(db, 'users', name + uid, 'data', 'summary'), {
+    summary: ''
+  })
+  await setDoc(doc(db, 'users', name + uid, 'data', 'extras'), {
+    extras: [
+    {
+      extraHeading: '',
+      extraItems: ['']
+    }
+    ]
+  })
 }
